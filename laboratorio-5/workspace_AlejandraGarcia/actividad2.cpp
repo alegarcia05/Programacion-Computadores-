637818df9e0e87fb8d7c3cd79d8131559856e5a8
Alejandra García <a.garcia05@ufromail.cl>
2021-05-10 22:58:10 -0400

    ejecutar suma de dos numeros

--- laboratorio-5/workspace_AlejandraGarcia/actividad2.cpp
+++ laboratorio-5/workspace_AlejandraGarcia/actividad2.cpp
@@ -1,10 +1,11 @@
 #include <stdio.h>
- char primercaracter;
- char segundocaracter;
-
+ int variable1;
+ char variable2;
 
  int main(){
-     printf("Ingrese un caracter");
-     scanf("%d, &primercaracter\n ");
-     
+     printf("Por favor, ingrese una variable\n");
+     scanf("%c", &variable2);
+     variable1 = variable2;
+     printf("su codigo ASCII es %i", variable2);
+return 0;
  }
---
+++ laboratorio-5/workspace_AlejandraGarcia/actividad5.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int tarea1;
+int tarea2;
+int tarea3;
+int actitudinal;
+int prueba1;
+int prueba2;
+
+int main(){
+    printf("Ingrese la nota de la tarea1\n");
+    scanf("%i", &tarea1);
+    printf("Ingrese la nota de la tarea2\n");
+    scanf("%i", &tarea2);
+    printf("Ingrese la nota de la tarea3\n");
+    scanf("%i", &tarea3);
+    printf("Ingrese la nota de actitudinal\n");
+    scanf("%i", &actitudinal);
+     printf("Ingrese la nota de la prueba1\n");
+    scanf("%i", &prueba1);
+    printf("Ingrese la nota de la prueba2\n");
+    scanf("%i", &prueba2);
+}
+
+
+10% Tarea 1, 10% Tarea 2, 10% Tarea 3, 10% Actitudinal, 25% Prueba 1 y 35% Prueba 
