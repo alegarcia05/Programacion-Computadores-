0672287ec2084340410a7c65c87d7713ff24a98a
Alejandra García <a.garcia05@ufromail.cl>
2021-05-10 21:56:05 -0400

    ejecutar actividad 4

--- laboratorio-5/workspace_AlejandraGarcia/actividad3.cpp
+++ laboratorio-5/workspace_AlejandraGarcia/actividad3.cpp
@@ -1,15 +1,15 @@
 #include <stdio.h>
+
 float numero1;
 float numero2;
-float multiplicacion; 
+float multiplicacion;
 
 int main(){
-
-printf("Ingrese un numero\n");
-    scanf("%d, &numero1"); 
-    printf("Ingrese un numero\n");
-    scanf("%d, &numero2");
-multiplicacion= numero1*numero2;
-printf("El resultado de la multiplicacion es %d\n",multiplicacion); 
-return 0;
+    printf("Ingrese un decimal:\n");
+    scanf("%f", &numero1);
+    printf("Ingrese un decimal:\n");
+    scanf("%f", &numero2);
+    multiplicacion = numero1 * numero2;
+    printf("el resultado de la multiplicacion es %f:\n", multiplicacion);
+    return 0;
 }
---
+++ laboratorio-5/workspace_AlejandraGarcia/actividad4.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+float radio;
+float perimetro;
+float area;
+float pi=3.14;
+
+int main(){
+     printf("Ingrese radio de la circunferencia\n");
+    scanf("%f, &radio"); 
+ perimetro= 2 * pi * radio;
+ area= pi * radio * radio;
+    printf("El perimetro de la circunferencia es %f\n, perimetro");
+     printf("El area de la circunferencia es %f\n, area");
+     return 0;
+}
