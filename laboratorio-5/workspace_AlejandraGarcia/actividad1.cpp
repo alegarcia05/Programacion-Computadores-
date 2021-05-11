c6c249feb5eb9c5f919de93a8adb6dfdc68b3857
Alejandra García <a.garcia05@ufromail.cl>
2021-05-10 20:20:40 -0400

    ejecutar actividad 1

---
+++ laboratorio-5/workspace_AlejandraGarcia/actividad1.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int numero1;
+int numero2; 
+int suma; 
+
+int main(){
+    printf("Ingrese un numero\n");
+    scanf("%d, &numero1"); 
+    printf("Ingrese un numero\n");
+    scanf("%d, &numero2");
+    suma=numero1+numero2;
+    printf("El resultado de la suma es %d\n",suma); 
+return 0;}
